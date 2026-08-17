5f22892c8427ca25440c6304f8cfc42635cf47b1
volpsz <76572284+volpszz@users.noreply.github.com>
2026-08-17 08:50:43 -0300

    Implement binary conversion for integer input
    Added variables for binary conversion and updated print statement.

--- lista1.c
+++ lista1.c
@@ -30,13 +30,32 @@
     
     EXERCICIO 3
 
-    int n;
+int n, resultado, bit64, bit32, bit16, bit8, bit4, bit2;
+
+printf("valor de n: ");
+scanf("%d",&n);
     
-    scanf("%d",&n);
-    
-    printf("valor em binario: %b",n);
-    
-    return 0;
+bit64 = n%2;
+resultado = n/2;
+
+bit32 = resultado%2;
+resultado = resultado/2;
+
+bit16 = resultado%2;
+resultado = resultado/2;
+   	
+bit8 = resultado%2;
+resultado = resultado/2;
+   	
+bit4 = resultado%2;
+resultado = resultado/2;   
+
+bit2 = resultado%2;
+resultado = resultado/2;
+   
+printf("o numero %d em binario = %d%d%d%d%d%d",n,resultado%2,bit2,bit4,bit8,bit16,bit32,bit64);  
+     
+return 0;
     
     */
     
