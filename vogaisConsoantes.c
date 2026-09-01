49269a3b4c02f5b902bac5ce3b9090e61c9fe4e3
volpsz <76572284+volpszz@users.noreply.github.com>
2026-09-01 08:57:39 -0300

    Modify letter checks and add switch cases
    Updated character range check for lowercase letters and added switch case for 'y', 'x', and 'z'.

--- vogaisConsoantes.c
+++ vogaisConsoantes.c
@@ -14,9 +14,22 @@ int main(){
     else if (letra == vogais[2] || letra == vogais[4]) {
         printf("La ele \n"); 
     }
-    else if (letra >= 'a' && letra <= 'z') {
+    else if (letra >= 'a' && letra <= 'w') {
         printf("67\n");
     }
 
+    switch (letra){
+        case 'y':
+        printf("EITCHAN JOAOOO!!!! KIT COPO");
+        break;
+
+        case 'x':
+        printf("EITCHAN COMO ELE TEM FORCA");
+        break;
+
+        case 'z':
+        printf("GASTOU 150 REAIS E ELE VAI ESTOURAR UM BALAO EBAAA");
+        break;
+    }
     return 0;
 }
